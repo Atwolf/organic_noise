9caccaac73e04f92e8a93d57dfc33d9f4de84a55
Atwolf <modernawe@gmail.com>
2023-09-06 16:45:46 -0500

    save

--- src/app.js
+++ src/app.js
@@ -5,63 +5,99 @@ import { SavePass } from 'three/examples/jsm/postprocessing/SavePass.js'
 import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js'
 import { BlendShader } from 'three/examples/jsm/shaders/BlendShader.js'
 import { CopyShader } from 'three/examples/jsm/shaders/CopyShader.js'
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js'
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js'
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js'
 
 import vertexShader from './shaders/vertex.glsl'
 import fragmentShader from './shaders/fragment.glsl'
 
-import colorTexture from './images/img.jpg'
+import vertexPars from './shaders/vertex_pars.glsl'
+import vertexMain from './shaders/vertex_main.glsl'
+
+import fragmentMain from './shaders/fragment_main.glsl'
+import fragmentPars from './shaders/fragment_pars.glsl'
 
 const startApp = () => {
   const scene = useScene()
   const camera = useCamera()
   const gui = useGui()
   const { width, height } = useRenderSize()
-
+  // loadFont()
   // settings
   const MOTION_BLUR_AMOUNT = 0.725
 
   // lighting
-  const dirLight = new THREE.DirectionalLight('#ffffff', 0.75)
-  dirLight.position.set(5, 5, 5)
+  const dirLight = new THREE.DirectionalLight('#526cff', 0.6)
+  dirLight.position.set(2, 2, 2)
 
-  const ambientLight = new THREE.AmbientLight('#ffffff', 0.2)
+  const ambientLight = new THREE.AmbientLight('#4255ff', 0.5)
   scene.add(dirLight, ambientLight)
 
+  // font
 
+  var loader = new FontLoader()
+  var pivot;
+  var text;
+  loader.load("fonts/helvetiker_regular.typeface.json", (font) => {
+    console.log("aasdlkjhasld")
+    var dummy = new THREE.MeshBasicMaterial({color: '0x00b294',wireframe:'true'})
+    obj = new TextGeometry("AT", {
 
-
-
-
-
-
-
-
-
+      font: font,
+      size: 20,
+      height: 5,
+      curveSegments: 1,
+      bevelEnabled: false
+    });
+    
+    console.log(font)
+    text = new THREE.Mesh( obj, dummy );
+    text.position.x = 0;
+    text.position.y = 0;
+    text.position.z = 0;
+    // text.rotation.y = 13
+    scene.add(text)
+  })
+  
   // meshes
-  const geometry = new THREE.PlaneGeometry(1)
-  const material = new THREE.ShaderMaterial({
-    vertexShader: vertexShader,
-    fragmentShader: fragmentShader
+  const geometry = new THREE.IcosahedronGeometry(0,400)
+  const material = new THREE.MeshStandardMaterial({
+    onBeforeCompile: (shader) => {
+      // store shader reference
+      material.userData.shader = shader
+
+      shader.uniforms.uTime = {value : 0}
+      
+      const parsVertexString = /* glsl */ `#include <displacementmap_pars_vertex>`
+      shader.vertexShader = shader.vertexShader.replace(
+        parsVertexString,
+        parsVertexString + vertexPars
+      )
+
+      const mainVertexString = /* glsl */ `#include <displacementmap_vertex>`
+      shader.vertexShader = shader.vertexShader.replace(mainVertexString, mainVertexString + vertexMain)
+
+      const mainFragmentString = /* glsl */ `#include <normal_fragment_maps>`
+      const parsFragmentString = /* glsl */ `#include <bumpmap_pars_fragment>`
+      shader.fragmentShader = shader.fragmentShader.replace(
+        parsFragmentString,
+        parsFragmentString + fragmentPars
+      )
+      shader.fragmentShader = shader.fragmentShader.replace(
+        mainFragmentString,
+        mainFragmentString + fragmentMain
+      )
+    }
   })
 
   const ico = new THREE.Mesh(geometry, material)
   scene.add(ico)
 
-  material.uniforms.uRadius = {value: 0.5}
-  material.uniforms.uTexture = {value: new THREE.TextureLoader().load(colorTexture)}
-
-
-
-
-
-
-
-
-
   // GUI
   const cameraFolder = gui.addFolder('Camera')
   cameraFolder.add(camera.position, 'z', 0, 10)
-  cameraFolder.add(material.uniforms.uRadius,"value",0,1).min(0).max(1)
+  // cameraFolder.add(material.uniforms.uRadius,"value",0,1).min(0).max(1)
 
   cameraFolder.open()
 
@@ -73,24 +109,9 @@ const startApp = () => {
     stencilBuffer: false,
   }
 
-  // save pass
-  const savePass = new SavePass(new THREE.WebGLRenderTarget(width, height, renderTargetParameters))
-
-  // blend pass
-  const blendPass = new ShaderPass(BlendShader, 'tDiffuse1')
-  blendPass.uniforms['tDiffuse2'].value = savePass.renderTarget.texture
-  blendPass.uniforms['mixRatio'].value = MOTION_BLUR_AMOUNT
-
-  // output pass
-  const outputPass = new ShaderPass(CopyShader)
-  outputPass.renderToScreen = true
-
-  // adding passes to composer
-  addPass(blendPass)
-  addPass(savePass)
-  addPass(outputPass)
-
+  addPass(new UnrealBloomPass(new THREE.Vector2(width,height), 0.7, 0.4, 0.4))
   useTick(({ timestamp, timeDiff }) => {
+    material.userData.shader.uniforms.uTime.value = timestamp / 5000
   })
 }
 
---
+++ src/shaders/vertex_main.glsl
@@ -0,0 +1,15 @@
+
+
+// noise
+vec3 coords = normal;
+coords.y += uTime;
+vec3 noisePattern = vec3(noise(coords / 1.5));
+float pattern = wave(noisePattern + uTime);
+
+// varyings
+vDisplacement = pattern;
+
+// MVP
+float displacement = vDisplacement / 3.0;
+
+transformed += normalize(objectNormal) * displacement;
